adcbb4ac8cbbe8677a5c5cfae939f4994d21ac01
Noor-Mustafa123 <77202386+Noor-Mustafa123@users.noreply.github.com>
2024-06-12 15:22:55 +0500

    Delete auth-provider-application directory

--- auth-provider-application/src/main/java/spring/Authorization/auth/provider/application/Entities/TokenType.java
+++
@@ -1,7 +0,0 @@
-package spring.Authorization.auth.provider.application.Entities;
-
-public enum TokenType {
-    BEARER
-}
-
-//We created an enum extra because so later on we can also add more type of tokens
